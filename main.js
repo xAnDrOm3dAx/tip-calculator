4d11cb19847d8518a09e81fa833e0b4e603a796a
Christopher Werkmeister <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-11-30 08:51:09 +1000

    handle tip percentage

--- main.js
+++ main.js
@@ -1,42 +1,53 @@
 const billInput = document.querySelector("#bill");
-const tipButtons = document.querySelector(".button-container");
+const tipButtons = document.querySelectorAll("[data-number]");
 const customTip = document.querySelector("#custom-input");
 const numOfPeople = document.querySelector("#num-of-people");
 const tipAmount = document.querySelector("#outcome-a");
 const totalAmount = document.querySelector("#outcome-b");
 const resetButton = document.querySelector("#reset");
 
+// Global Variables
+let tipPercentage = ""; // Variable to store the clicked button's text content
+
+//Event Listeners
+resetButton.addEventListener("click", reset);
+
+tipButtons.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    tipPercentage = e.target.textContent.slice(0, -1); // Remove the last character (which is '%');
+    console.log(tipPercentage);
+  });
+});
+
 // Array of input elements
 const inputElements = [billInput, numOfPeople, customTip];
 
 // Function to handle keydown events for input fields
-const handleInput = (inputField, e) => {
+function handleInput(inputField, e) {
   const invalidChars = ["-", "+", "e"];
 
   if (invalidChars.includes(e.key)) {
     e.preventDefault();
   }
-};
+}
 
-const addInputEventListener = (inputElement) => {
+function addInputEventListener(inputElement) {
   inputElement.addEventListener("keydown", (e) => {
     handleInput(inputElement, e);
   });
-};
+}
 
-// Adding event listeners using the function
-inputElements.forEach(addInputEventListener);
-resetButton.addEventListener("click", reset);
+inputElements.forEach(addInputEventListener); // Adding event listeners using the function
 
-// Other functions
 function reset() {
   billInput.value = "";
   numOfPeople.value = "";
+  customTip.value = "";
   tipAmount.textContent = "$0.00";
   totalAmount.textContent = "$0.00";
 }
 
-function calculateTip() {
+function calculate() {
   /* 
 tip amount = bill amount * (tip percentage / 100)
 tip amount per person = tip amount / number of people
