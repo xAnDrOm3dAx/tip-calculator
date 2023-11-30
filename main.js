2712c3b2a0c1f89b0db9345a1b9c98167d7386ad
Christopher Werkmeister <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-11-30 14:43:44 +1000

    add eventlistener & calculate logic + clean up

--- main.js
+++ main.js
@@ -3,41 +3,40 @@ const tipButtons = document.querySelectorAll("[data-number]");
 const customTip = document.querySelector("#custom-input");
 const numOfPeople = document.querySelector("#num-of-people");
 const numOfPeopleAlert = document.querySelector("#cant-be-zero");
-const tipAmount = document.querySelector("#outcome-a");
-const totalAmount = document.querySelector("#outcome-b");
+let tipAmount = document.querySelector("#outcome-a");
+let totalAmount = document.querySelector("#outcome-b");
 const resetButton = document.querySelector("#reset");
 
 // Default values
-let tipPercentage = ""; // Variable to store the clicked button's and custom text content
 let billValue = 0.0;
 let numOfPeopleValue = 1;
+let tipPercentage = ""; // Variable to store the clicked button's and custom text content
 
 //Event Listeners
+numOfPeople.addEventListener("input", cantBeZeroAlert);
+
+resetButton.addEventListener("click", reset);
+
 billInput.addEventListener("keyup", (e) => {
   billValue = Number(e.target.value);
-  console.log(billValue);
 });
 
 numOfPeople.addEventListener("keyup", (e) => {
-  // numOfPeopleValue = 1;
   numOfPeopleValue = Number(e.target.value);
-  console.log(numOfPeopleValue);
-});
-
-customTip.addEventListener("keyup", (e) => {
-  tipPercentage = "";
-  tipPercentage = Number(e.target.value);
-  console.log(tipPercentage);
-  calculate();
 });
 
 tipButtons.forEach((button) => {
   button.addEventListener("click", function (e) {
     tipPercentage = "";
     tipPercentage = e.target.textContent.slice(0, -1); // Remove the last character (%)
-    console.log(tipPercentage);
     calculate();
   });
+});
+
+customTip.addEventListener("keyup", (e) => {
+  tipPercentage = "";
+  tipPercentage = Number(e.target.value);
+  calculate();
 });
 
 // Handle keydown events for input fields
@@ -47,6 +46,7 @@ inputElements.forEach((inputElement) => {
   inputElement.addEventListener("keydown", validateInput);
 }); // Add keydown event listener to all input elements
 
+// Functions
 function validateInput(e) {
   const invalidChars = ["-", "+", "e"];
 
@@ -64,7 +64,6 @@ function cantBeZeroAlert() {
     numOfPeopleAlert.style.visibility = "hidden";
   }
 }
-numOfPeople.addEventListener("input", cantBeZeroAlert);
 
 function reset() {
   billInput.value = "";
@@ -74,36 +73,17 @@ function reset() {
   totalAmount.textContent = "$0.00";
   numOfPeopleAlert.style.visibility = "hidden";
 }
-resetButton.addEventListener("click", reset);
 
 function calculate() {
-  let totalPerPerson = billValue / numOfPeopleValue;
-  console.log(totalPerPerson);
+  let totalTipAmount = billValue * (tipPercentage / 100);
+  let totalAmountPerPerson = billValue / numOfPeopleValue;
+  let totalTipPerPerson = totalTipAmount / numOfPeopleValue;
+  let perPersonAmount = totalAmountPerPerson + totalTipPerPerson;
 
-  // if numOfPeopleValue = 0 - return
-
-  // if (billValue >= 1) {
-  //   totalAmount.textContent = (billValue * tipPercentage) / numOfPeople;
-  //   let total = (billValue * (tipValue + 1)) / peopleValue;
-  //   results[0].innerHTML = "$" + tipAmount.toFixed(2);
-  //   results[1].innerHTML = "$" + total.toFixed(2);
-  // }
-  /* 
-tip amount = bill amount * (tip percentage / 100)
-tip amount per person = tip amount / number of people
-
-total amount = bill amount + tip amount
-total per person = total amount / number of people
-*/
-  // let tipAmount = billAmount * (tipPercentage / 100);
-  // let totalAmount = billAmount + tipAmount;
-  // let perPersonTip = tipAmount / numOfPeople;
-  // let perPersonAmount = totalAmount / numOfPeople;
+  if (numOfPeopleValue >= 1) {
+    tipAmount.textContent = `$${totalTipPerPerson.toFixed(2)}`;
+    totalAmount.textContent = `$${perPersonAmount.toFixed(2)}`;
+  } else {
+    return;
+  }
 }
-
-// customTip.addEventListener("keyup", () => {
-//     tipPercent = custom.value;
-//     const tipAmount = bill.value * (tipPercent /100)
-// });
-
-console.log(tipPercentage);
