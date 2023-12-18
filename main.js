37866ff72887c4c4a170972cbfd16eabce49728b
Christopher Werkmeister <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-12-18 11:38:31 +1000

    clean-up

--- main.js
+++ main.js
@@ -60,24 +60,6 @@ function validateInput(e) {
   e.target.value = cleanedValue;
 }
 
-/*
-// Handle keydown events for input fields
-const inputElements = [billInput, numOfPeople, customTip];
-
-inputElements.forEach((inputElement) => {
-  inputElement.addEventListener("keydown", validateInput);
-}); // Add keydown event listener to all input elements
-
-// Functions
-function validateInput(e) {
-  const invalidChars = ["-", "+", "e"];
-
-  if (invalidChars.includes(e.key)) {
-    e.preventDefault();
-  }
-}
-*/
-
 function cantBeZeroAlert() {
   const numOfPeopleValue = parseInt(numOfPeople.value);
 
@@ -111,24 +93,3 @@ function calculate() {
     return;
   }
 }
-
-// refactor validateInput to work with iOS and mac. Remove number and replace with text? Validate with RGX.
-// Limit maxlength of user input
-// Buttons do not stay highlighted when user clicks away
-// Do I need default values? And do I need to reset the top percentages here?
-
-/* 
-tipButtons.forEach((button) => {
-  button.addEventListener("click", function (e) {
-    tipPercentage = "";
-    tipPercentage = e.target.textContent.slice(0, -1); // Remove the last character (%)
-    calculate();
-  });
-});
-
-customTip.addEventListener("keyup", (e) => {
-  tipPercentage = "";
-  tipPercentage = Number(e.target.value);
-  calculate();
-});
-*/
